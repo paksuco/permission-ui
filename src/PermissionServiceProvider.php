fde4f5d4047ad14bda47d0795b05157c855bc664
Taha Paksu <tpaksu@gmail.com>
2020-08-28 00:39:39 +0300

    styles

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -31,8 +31,8 @@ class PermissionServiceProvider extends ServiceProvider
 
         Event::listen("paksuco.menu.beforeRender", function ($key, $container) {
             if ($key == "admin") {
-                if ($container->hasItem("Permission") == false) {
-                    $container->addItem("Permission", route("paksuco.permissions"), "fa fa-lock");
+                if ($container->hasItem("Permissions") == false) {
+                    $container->addItem("Permissions", route("paksuco.permissions"), "fa fa-lock");
                 }
             }
         });
