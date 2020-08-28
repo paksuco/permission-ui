5562adaca7a0f971eea74222927a26014b827286
Taha Paksu <taha.paksu@edata.com.tr>
2020-08-28 16:57:35 +0300

    layout

--- views/components/theme-column/partials/search.blade.php
+++ views/components/theme-column/partials/search.blade.php
@@ -2,7 +2,7 @@
     <input wire:model.debounce.500ms="searchKeyword"
         class="bg-gray-200 rounded shadow
             placeholder-gray-800 py-2 px-3 text-gray-700 flex-1 leading-tight
-            focus:bg-white min-w-0 relative text-sm"
+            focus:bg-white min-w-0 relative text-sm focus:outline-none"
         type="text" placeholder="@lang('Role name')">
     <i class="fa fa-search absolute right-2 flex text-gray-500 justify-end items-center"></i>
 </div>
