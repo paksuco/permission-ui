6f774851a649de0bc19d3efa2ba8fbda65767beb
tahapaksu <taha@nox.ist>
2021-04-07 07:57:27 +0000

    remove redundant border

--- views/components/theme-column/table.blade.php
+++ views/components/theme-column/table.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col flex-1">
     <div class="flex flex-wrap flex-1">
-        <div class="flex-shrink-0 w-full p-4 border-t lg:w-60">
+        <div class="flex-shrink-0 w-full p-4 border-t lg:w-80">
             <div class="flex flex-col min-h-0">
                 <h3 class="pb-2 text-base font-semibold">@lang("Search")</h3>
                 @include("permission-ui::components.theme-column.partials.search")
@@ -10,7 +10,7 @@
                 @include("permission-ui::components.theme-column.partials.new-role")
             </div>
         </div>
-        <div class="flex-1 p-8 bg-white border-t shadow-xl">
+        <div class="flex-1 p-8 bg-white">
             <div class="items-end w-full pb-8">
                 <h2 class="mb-3 text-3xl font-semibold" style="line-height: 1em">@lang("Role Management")</h2>
                 <p class="mb-4 text-sm font-light leading-5 text-gray-600">@lang("This page contains the available permissions and user roles. You can create a new role, assign a permission to a role, and use that role to limit an user^s authorized actions.")</p>
